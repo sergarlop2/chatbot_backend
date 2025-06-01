b7784060265104252f62132bc2d449eb98570cb6
sergarlop2 <sergarlop2@alum.us.es>
2025-06-01 19:53:00 +0200

    Fix CORS

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ hf_pipeline = HuggingFacePipeline(pipeline=llm_pipe)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
